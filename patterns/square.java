092e75dac5efa8d303725b732f9a9f5f4b660e87
sanju51 <sanjanabu47@gmail.com>
2024-10-07 01:17:27 +0530

    tt

---
+++ patterns/square.java
@@ -0,0 +1,14 @@
+public class square {
+    public static void main(String[] args) {
+        int j,i;
+        for(i=1;i<=10;i++){
+            for(j=1;j<=7;j++){
+                if(i==1||i==10||j==1||j==7){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+        }System.out.println();
+}
+    }}
