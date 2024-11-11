94437de7e83279cc6224961aaacf099238ff38db
sanju51 <sanjanabu47@gmail.com>
2024-11-12 00:54:54 +0530

    ii

--- arrays/sorting/bubble.java
+++ arrays/sorting/bubble.java
@@ -19,7 +19,7 @@ public class bubble {
     }}
     public static int[] sort(int arr[]){
         int n = arr.length;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-2;i++){
             for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
@@ -34,3 +34,5 @@ public class bubble {
 
     
 }
+
+//yrr mein nahi reh sakti hu tere binna mein kya bolu ye pyaar feelings ye sab ek tamasha nahi hai yrr pta nahi aise ajeeb sa fell ho raha hai bohot darr lag raha hai yaar yaar kabhi mat chodke jaana yrr mein loyal rahungi hamesha yrr .. tereko chodke ye dosti ye sab kuch bhi nahi hai yrr mein nhi reh sakti yrr i am sorry yaar mein tere har problem mein support karungi aur hamesha tere saath rahungi yaar maaf kardo yrr mujhe please i am sorry
