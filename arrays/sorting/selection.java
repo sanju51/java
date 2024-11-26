60c4feacf95a5574d0d573f152d02bef3154b09a
sanju51 <sanjanabu47@gmail.com>
2024-11-27 01:17:34 +0530

    kk

---
+++ arrays/sorting/selection.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class selection {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of n");
+        int n =sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter "+n+" elements");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        sort(arr);
+        for(int i=0;i<n;i++){
+            System.out.print(" "+arr[i]);
+    
+    }}
+
+    public static int[] sort(int []arr){
+        for(int i=0;i<arr.length;i++){
+            int min =i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+            int temp = arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }return arr;
+    }
+}
