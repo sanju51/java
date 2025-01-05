3a598455ace57f3d511f00e8aff794fcebdf6e1f
sanju51 <sanjanabu47@gmail.com>
2025-01-06 02:34:23 +0530

    kk

---
+++ strings/sample.java
@@ -0,0 +1,11 @@
+
+
+public class sample {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String str2 = "sanjana";
+        String result = str+ " " +str2;
+        System.out.println(result+ " How are you!!");
+    
+    
+}}
