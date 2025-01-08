d59e70144b203c0d64564a7cc0086bb876fa2cd0
sanju51 <sanjanabu47@gmail.com>
2025-01-09 01:44:24 +0530

    ger

--- substring.java
+++ substring.java
@@ -3,6 +3,14 @@ public class substring {
         String str = "Hello, World!";
         String sub = str.substring(7);
         System.out.println(sub);
+        substring(str,3,9);
         }
-    
+        public static void substring(String str, int start, int end) {
+            String hu="";
+            for(int i=start; i<end ; i++)
+            {
+                hu += str.charAt(i);
+            }
+            System.out.println(hu);
+        }
 }
