724e7b506ac27c8672cac860b811324124d6c5e0
sanju51 <sanjanabu47@gmail.com>
2025-01-10 23:47:41 +0530

    kkdkd

--- substring.java
+++ substring.java
@@ -1,8 +1,18 @@
+
+
 public class substring {
     public static void main(String[] args) {
         String str = "Hello, World!";
         String sub = str.substring(7);
         System.out.println(sub);
+        
+        String res[]={"apple","banana","zygh","orange"};
+        String largest=res[0];
+        for(int i=1; i< res.length; i++){
+            if(largest.compareTo(res[i])<0){
+            largest=res[i];
+            }
         }
-    
-}
+        System.out.println(largest);
+
+}}
